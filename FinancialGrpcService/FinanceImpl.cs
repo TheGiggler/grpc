9f5e8adf378e0debcf35935e718a2c15fe1df527
Mike Tocci <maddog19rw@gmail.com>
2019-04-09 22:44:39 -0400

    initial commit

---
+++ FinancialGrpcService/FinanceImpl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Utils;
+
+namespace Financial
+{
+    public class FinanceImpl: FinancialTransaction.FinancialTransactionBase
+    {
+        public override Task<TransactionResponse> ProcessTransaction(TransactionRequest request, ServerCallContext context)
+        {
+            return base.ProcessTransaction(request, context);
+        }
+
+        public override Task ProcessTransactionStream(IAsyncStreamReader<TransactionRequest> requestStream, IServerStreamWriter<TransactionResponse> responseStream, ServerCallContext context)
+        {
+            return base.ProcessTransactionStream(requestStream, responseStream, context);
+        }
+    }
+}
---
+++ FinancialGrpcService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Grpc.Core;
+using Grpc.Core.Utils;
+using Financial;
+
+namespace FinancialGrpcService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+          }
+    }
+}
